42f171141a5989a2b482f33386403e40e92cdd6e
nat1998-k <77759698+nat1998-k@users.noreply.github.com>
2021-11-26 19:33:37 -0500

    Merge branch 'main' of https://github.com/sarahVacca/220project

--- django/djangoproject/djangoproject/urls.py
+++ django/djangoproject/djangoproject/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', lambda req: redirect('clarkclothes/')),
     path('clarkclothes/', include('clarkclothes.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('additem/', include('clarkclothes.urls'))
 
