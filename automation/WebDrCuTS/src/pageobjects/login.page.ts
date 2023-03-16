6f9939838f2ce02407afca3e784d04d8b822be96
sarahVacca <sarahvacca3@gmail.com>
2023-03-16 17:52:22 -0400

    Automation update

---
+++ automation/WebDrCuTS/src/pageobjects/login.page.ts
@@ -0,0 +1,10 @@
+import Page from './page';
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class LoginPage extends Page {
+   
+}
+
+export default new LoginPage();
